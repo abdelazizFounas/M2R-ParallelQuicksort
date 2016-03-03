6c09da9b6f87fa65ab5ae5e0b611843e79375ef3
Abdelaziz Founas <founasabd@gmail.com>
2016-03-03 16:31:01 +0100

    Fix run_benchmarking.sh Fix csv_quicksort_extractor.pl
    About result_csv, result_1.Rmd et measurements

--- scripts/run_benchmarking.sh
+++ scripts/run_benchmarking.sh
@@ -1,12 +1,12 @@
 
-OUTPUT_DIRECTORY=data/`hostname`_`date +%F`
+OUTPUT_DIRECTORY=../data/AF_Datas/
 mkdir -p $OUTPUT_DIRECTORY
-OUTPUT_FILE=$OUTPUT_DIRECTORY/measurements_`date +%R`.txt
+OUTPUT_FILE=$OUTPUT_DIRECTORY/measurements_`date +%d-%m-%Y_%H-%M-%S`.txt
 
 touch $OUTPUT_FILE
 for i in 100 1000 10000 100000 1000000; do
     for rep in `seq 1 5`; do
         echo "Size: $i" >> $OUTPUT_FILE;
-        ./src/parallelQuicksort $i >> $OUTPUT_FILE;
+        ../src/parallelQuicksort $i >> $OUTPUT_FILE;
     done ;
 done
